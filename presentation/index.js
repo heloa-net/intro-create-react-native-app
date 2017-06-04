bda36d6616d7f1ad49c520d2741a0d87d49d2328
Heloá Meneses <heloa.meneses@gmail.com>
2017-06-03 22:10:54 -0300

    Updating slides from intro to demo

--- presentation/index.js
+++ presentation/index.js
@@ -3,10 +3,14 @@ import React from "react";
 
 // Import Spectacle Core tags
 import {
+  Appear,
   BlockQuote,
   Cite,
+  CodePane,
   Deck,
   Heading,
+  Image,
+  Link,
   ListItem,
   List,
   Quote,
@@ -26,19 +30,20 @@ require("spectacle/lib/themes/default/index.css");
 
 
 const images = {
-  city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
+  crying: require("../assets/crying.jpg"),
+  feels: require("../assets/feels.jpg"),
+  fine: require("../assets/fine.jpg"),
+  omg: require("../assets/omg.jpg"),
+  title: require("../assets/title.jpg")
 };
 
 preloader(images);
 
 const theme = createTheme({
   primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quartenary: "#CECECE"
+  secondary: "#d645bb",
+  tertiary: "#1F2022",
+  quaternary: "#CECECE"
 }, {
   primary: "Montserrat",
   secondary: "Helvetica"
@@ -48,38 +53,77 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
-          </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
-          </Text>
-        </Slide>
+
+        <Slide transition={["zoom"]} bgImage={images.title} />
+
         <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+          <Heading size={4} textColor="primary" caps>What & why?</Heading>
+          <Appear>
+            <Image src={images.crying} />
+          </Appear>
         </Slide>
+
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
+          <Link href="https://github.com/react-community/create-react-native-app" target="_blank">
+            <Heading size={4} textColor="tertiary">Create React Native App</Heading>
+          </Link>
+          <Appear>
+            <Heading size={4}>🎉</Heading>
+          </Appear>
+          <Appear>
+            <List>
+              <ListItem>Facebook & Expo</ListItem>
+              <ListItem>create-react-app</ListItem>
+              <Link href="https://facebook.github.io/react-native/blog/2017/03/13/introducing-create-react-native-app.html" target="_blank">
+                <ListItem>⚡Talk at React Conf '17</ListItem>
+              </Link>
+            </List>
+          </Appear>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Link href="https://github.com/react-community/create-react-native-app" target="_blank">
+            <Heading size={4} textColor="tertiary">Create React Native App</Heading>
+          </Link>
+          <Heading size={4}>🎉</Heading>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <ListItem>Minimal setup</ListItem>
+            <ListItem>Test on your mobile</ListItem>
+            <ListItem>Get started quickly</ListItem>
           </List>
         </Slide>
+
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Image src={images.fine} />
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="tertiary">
+            <Link href="https://nodejs.org/en/download/" target="_blank">
+              <Heading size={4} textColor="primary">node</Heading>
+            </Link>
+            <Heading size={4} textColor="primary">+</Heading>
+            <Link href="https://expo.io/tools" target="_blank">
+              <Heading size={4} textColor="primary">
+                Expo 📱
+              </Heading>
+            </Link>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <CodePane lang="bash" source={require("raw-loader!../assets/crna-installation.example")} />
+          <Appear>
+            <CodePane lang="bash" source={require("raw-loader!../assets/crna-installation-npm.example")} />
+          </Appear>
+        </Slide>
+
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
+            <Quote>OMG it's demo time</Quote>
+            <Cite>My brain right now</Cite>
           </BlockQuote>
         </Slide>
+
+
       </Deck>
     );
   }
